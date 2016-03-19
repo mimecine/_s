9ff9445059bbdf794f68e9f26f292e52a3d05041
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-19 15:39:13 +0200

    add a typography section, showcase adding things.

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,72 @@ function _s_customize_preview_js() {
 	wp_enqueue_script( '_s_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', '_s_customize_preview_js' );
+
+/**
+ * Add the theme configuration
+ */
+_s_Kirki::add_config( '_s_theme', array(
+	'option_type' => 'theme_mod',
+	'capability'  => 'edit_theme_options',
+) );
+
+/**
+ * Add the typography section
+ */
+_s_Kirki::add_section( 'typography', array(
+	'title'      => esc_attr__( 'Typography', '_s' ),
+	'priority'   => 2,
+	'capability' => 'edit_theme_options',
+) );
+
+/**
+ * Add the body-typography control
+ */
+_s_Kirki::add_field( '_s_theme', array(
+	'type'        => 'typography',
+	'settings'    => 'body_typography',
+	'label'       => esc_attr__( 'Body Typography', 'kirki' ),
+	'description' => esc_attr__( 'Select the main typography options for your site.', '_s' ),
+	'help'        => esc_attr__( 'The typography options you set here apply to all content on your site.', '_s' ),
+	'section'     => 'typography',
+	'priority'    => 10,
+	'default'     => array(
+		'font-family'    => 'Roboto',
+		'variant'        => '400',
+		'font-size'      => '16px',
+		'line-height'    => '1.5',
+		// 'letter-spacing' => '0',
+		'color'          => '#333333',
+	),
+	'output' => array(
+		array(
+			'element' => 'body',
+		),
+	),
+) );
+
+/**
+ * Add the body-typography control
+ */
+_s_Kirki::add_field( '_s_theme', array(
+	'type'        => 'typography',
+	'settings'    => 'headers_typography',
+	'label'       => esc_attr__( 'Headers Typography', 'kirki' ),
+	'description' => esc_attr__( 'Select the typography options for your headers.', '_s' ),
+	'help'        => esc_attr__( 'The typography options you set here will override the Body Typography options for all headers on your site (post titles, widget titles etc).', '_s' ),
+	'section'     => 'typography',
+	'priority'    => 10,
+	'default'     => array(
+		'font-family'    => 'Roboto',
+		'variant'        => '400',
+		// 'font-size'      => '16px',
+		// 'line-height'    => '1.5',
+		// 'letter-spacing' => '0',
+		// 'color'          => '#333333',
+	),
+	'output' => array(
+		array(
+			'element' => array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', '.h1', '.h2', '.h3', '.h4', '.h5', '.h6' ),
+		),
+	),
+) );
