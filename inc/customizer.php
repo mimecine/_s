ac4bbe7a9fe3c44a966a5cd8a76d39190f690f68
WPDevHQ <git@wpdevhq.com>
2016-04-21 15:53:25 +0100

    Fixes textdomain and hide description
    Fixes the incorrect textdomain in customizer.php and adds styles to hid
    the description when plugin is installed but not active.

--- inc/customizer.php
+++ inc/customizer.php
@@ -48,7 +48,7 @@ _s_Kirki::add_section( 'typography', array(
 _s_Kirki::add_field( '_s_theme', array(
 	'type'        => 'typography',
 	'settings'    => 'body_typography',
-	'label'       => esc_attr__( 'Body Typography', 'kirki' ),
+	'label'       => esc_attr__( 'Body Typography', '_s' ),
 	'description' => esc_attr__( 'Select the main typography options for your site.', '_s' ),
 	'help'        => esc_attr__( 'The typography options you set here apply to all content on your site.', '_s' ),
 	'section'     => 'typography',
@@ -74,7 +74,7 @@ _s_Kirki::add_field( '_s_theme', array(
 _s_Kirki::add_field( '_s_theme', array(
 	'type'        => 'typography',
 	'settings'    => 'headers_typography',
-	'label'       => esc_attr__( 'Headers Typography', 'kirki' ),
+	'label'       => esc_attr__( 'Headers Typography', '_s' ),
 	'description' => esc_attr__( 'Select the typography options for your headers.', '_s' ),
 	'help'        => esc_attr__( 'The typography options you set here will override the Body Typography options for all headers on your site (post titles, widget titles etc).', '_s' ),
 	'section'     => 'typography',
